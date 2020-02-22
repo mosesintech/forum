28b21485f236692b3c3fa01a13ba470ea7bb0e88
Moses Cosme <mosesmaxcosme@gmail.com>
2020-02-22 13:03:01 -0600

    added middleware files

--- server/README.md
+++ server/README.md
@@ -78,3 +78,29 @@ DELETE is exclusively an Administrator action.
 ## Database Model
 
 ![alt text](../images/database/database-refactored.png "Database Model")
+
+
+<!-- exports.up = function(knex) {
+  return knex.schema.createTable('users', users => {
+  users.increments();
+  users.boolean('is_admin').notNullable().defaultTo(false);
+  users.boolean('is_mod').notNullable().defaultTo(false);
+  users.boolean('is_banned').notNullable().defaultTo(false);
+  users.timestamp('joined_date').defaultTo(knex.fn.now()).notNullable();
+  users.text('username', 30).notNullable();
+  users.text('avatar').notNullable().defaultTo('https://avatarfiles.alphacoders.com/219/219973.jpg');
+  users.text('user_title', 30).defaultTo('New User');
+  users.text('email', 30).notNullable();
+  users.text('password', 30).notNullable();
+  users.date('birthday');
+  users.text('website', 30);
+  users.text('sex', 10);
+  users.text('location', 30);
+  users.text('bio', 128);
+  users.text('signature', 128);
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+}; -->
--- server/knexfile.js
+++ server/knexfile.js
@@ -2,45 +2,48 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      filename: './data/forum.db3'
+      host: 'localhost',
+      database: 'forum',
+      // owner: 'maxcosme',
+      // password: 'admin21'
     },
     migration: {
-      directory: "./data/migration"
+      directory: "./data/migrations"
     },
     seeds: {
       directory: "./data/seeds"
     }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
 };
